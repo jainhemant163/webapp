4cc3a00b54dd36025c3e06855b72b517e4821163
jainhemant163 <jainhemant163@gmail.com>
2020-01-29 10:29:16 -0500

    SQ Lfix

--- sql-data/dummy.sql
+++ sql-data/dummy.sql
@@ -17,3 +17,46 @@ VALUES (
 	"FooBazBar City"
 );
 
+use users_database;
+
+DROP TABLE IF EXISTS `users`;
+create table users
+(
+    id              varchar(256) not null,
+    email           varchar(128)  not null,
+    password        varchar(256) not null,
+    firstName       varchar(100)     null,
+    lastName        varchar(100)     null,
+    account_created varchar(100)     null,
+    account_updated varchar(100)     null,
+    constraint users_pk
+        unique (id)
+);
+
+insert into users(email,password,firstName,lastName,account_created,account_updated,id) values("11@qq.com",
+"$2y$10$sTEjAha0zxFNV4EMRSuBze1l1kAhLQrENe/ICZG4zfjWJwkorUfq","HJ","MJ","2018-01-01","2018-01-01",3);
+
+DROP TABLE IF EXISTS `Bill`;
+
+create table Bill
+(
+    id                    varchar(256) not null primary key unique ,
+    created_ts            varchar(100) null,
+    updated_ts            varchar(100) null,
+    owner_id             varchar(256) null,
+    vendor                 varchar(100) null,
+    bill_date                varchar(100) null,
+    due_date                varchar(100) null,
+    amount_due             double null,
+    categories              TEXT,
+    paymentStatus          varchar(100) null
+
+);
+
+
+insert into Bill(id,created_ts,updated_ts,owner_id,vendor,bill_date,due_date,amount_due,categories,paymentStatus)
+values(5,"2010-01-01","2010-01-01",123456,"JAck","2010-01-01","2010-01-10",500,"A,B","due");
+
+
+
+
