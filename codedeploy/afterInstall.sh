758e0ac08d033b8aaa75af638db45e5b6ada08f7
jainhemant163 <jainhemant163@gmail.com>
2020-03-07 18:33:59 -0500

    DD

---
+++ codedeploy/afterInstall.sh
@@ -0,0 +1,8 @@
+  
+#!/bin/bash
+cd /home/ubuntu/webapp
+sudo chown -R ubuntu:ubuntu /home/ubuntu/webapp
+sudo chmod +x WebApplication-0.0.1-SNAPSHOT.jar
+source /etc/profile.d/envvariable.sh
+kill -9 $(ps -ef|grep WebApplication | grep -v grep | awk '{print$2}')
+nohup java -jar WebApplication-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt 2> /home/ubuntu/output.txt < /home/ubuntu/output.txt &
