43cc2bc1371802f2716507ecda54570669fd14df
jainhemant163 <jainhemant163@gmail.com>
2020-03-07 20:12:21 -0500

    dd

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,7 +1,7 @@
   
 #!/bin/bash
-cd /home/ubuntu/webapp
-sudo chown -R ubuntu:ubuntu /home/ubuntu/webapp
+cd /home/ubuntu/WebApplication
+sudo chown -R ubuntu:ubuntu /home/ubuntu/WebApplication
 sudo chmod +x WebApplication-0.0.1-SNAPSHOT.jar
 source /etc/profile.d/envvariable.sh
 kill -9 $(ps -ef|grep WebApplication | grep -v grep | awk '{print$2}')
