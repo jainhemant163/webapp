a1fd23a4d4c7bb5c0c3f698961a93f5896d41077
jainhemant163 <jainhemant163@gmail.com>
2020-03-12 18:18:09 -0400

    install

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -5,4 +5,4 @@ sudo chmod +x WebApplication-0.0.1-SNAPSHOT.jar
 source /etc/profile.d/envvariable.sh
 sudo systemctl stop tomcat8
 kill -9 $(ps -ef|grep WebApplication | grep -v grep | awk '{print$2}')
-nohup java -jar WebApplication-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt 2> /home/ubuntu/output.txt < /home/ubuntu/output.txt &
+nohup java $JAVA_OPTS -jar WebApplication-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt 2> /home/ubuntu/output.txt < /home/ubuntu/output.txt &
