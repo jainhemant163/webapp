8d93963d994dfb982c2685ea92510b4413aa2097
jainhemant163 <jainhemant163@gmail.com>
2020-03-18 11:23:11 -0400

    assignment8

--- WebApplication/src/main/java/com/cloud/csye6225/assignment/controller/MetricsClientConfig.java
+++ WebApplication/src/main/java/com/cloud/csye6225/assignment/controller/MetricsClientConfig.java
@@ -1,32 +1,40 @@
 package com.cloud.csye6225.assignment.controller;
 
 
-
-import com.timgroup.statsd.NoOpStatsDClient;
-import com.timgroup.statsd.NonBlockingStatsDClient;
-import com.timgroup.statsd.StatsDClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.timgroup.statsd.NonBlockingStatsDClient;
+import com.timgroup.statsd.StatsDClient;
+import com.timgroup.statsd.NoOpStatsDClient;
+
+/**
+ * Metric collection client class with StatsDClient
+ * 
+ * @author hrjain
+ *
+ */
 @Configuration
 public class MetricsClientConfig {
 
-    @Value("${publish.metrics}")
-    private boolean publishMetrics;
+	@Value("true")
+	private boolean publishMessage;
 
-    @Value("${metrics.server.hostname}")
-    private String metricsServerHost;
+	@Value("localhost")
+	private String metricHost;
 
-    @Value("${metrics.server.port}")
-    private int metricsServerPort;
+	@Value("8125")
+	private int portNumber;
 
-    @Bean
-    public StatsDClient metricsClient(){
-        if(publishMetrics){
-            return new NonBlockingStatsDClient("csye6225", metricsServerHost, metricsServerPort);
-        }
-        return new NoOpStatsDClient();
-    }
+	@Value("csye6225")
+	private String prefix;
 
+	@Bean
+	public StatsDClient metricClient() {
+		if (publishMessage)
+			return new NonBlockingStatsDClient(prefix, metricHost, portNumber);
+
+		return new NoOpStatsDClient();
+	}
 }
